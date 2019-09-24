86105bde08670d24bbc79008858e4b2de3bc4e34
Sai srinivas <Sai srinivas@LAPTOP-7HTBRT8Q>
2019-09-24 22:07:58 +0530

    first commit

---
+++ DataTypes/src/Data.java
@@ -0,0 +1,24 @@
+
+public class Data {
+	public static void main(String[] args) {
+	
+		byte b=-127;
+		short s = 32678;
+		int i= 658977897;
+		long l=698544789;
+		float f=20.25f;
+		double d=-5.8999777445265d;
+		
+		System.out.println("b= "+b);
+		System.out.println("s= "+s);
+		System.out.println("i= "+i);
+		System.out.println("l= "+l);
+		System.out.println("f= "+f);
+		System.out.println("d= "+d);
+
+		
+	}
+
+	
+		
+	}
